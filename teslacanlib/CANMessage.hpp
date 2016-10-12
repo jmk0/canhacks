2ddf178cc2f55828ae32f3ddbeb44c965c7e7302
jmk0 <thenaimis@gmail.com>
2016-10-11 20:04:52 -0500

    Make fixedDecode slightly more generalized, perhaps uselessly

--- teslacanlib/CANMessage.hpp
+++ teslacanlib/CANMessage.hpp
@@ -30,6 +30,40 @@ namespace canhacks
        * @param[in] scale A scale value to multiply the integer
        *   quantity by to get the correct precision of the engineering
        *   units value before returning.
+       * @param[in] leftShift The integer quantity will be shifted
+       *   left by this number of bits before being right shifted (see
+       *   rightShift).
+       * @param[in] rightShift The integer quantity will be shifted
+       *   right by this number of bits before being scaled and
+       *   returned.
+       * @param[in] offset A value added to the integer quantity prior
+       *   to scaling.
+       */
+   template <typename T>
+   double fixedDecode(uint8_t *startPtr, double scale, unsigned leftShift,
+                      unsigned rightShift, T offset)
+   {
+      T tmpValInt = *(T*)startPtr;
+      tmpValInt += offset;
+         // bit shift for sign extension or, for unsigned, crud removal
+      if (leftShift)
+         tmpValInt <<= leftShift;
+      if (rightShift)
+         tmpValInt >>= rightShift;
+      return tmpValInt * scale;
+   }
+
+
+      /** Generic decoding function for multi-byte quantities.  Can
+       * also be used for single-byte quantities, albeit somewhat less
+       * optimally.
+       * @tparam T The integer data type used for decoding prior to
+       *   scaling.  This should be a fixed bit size, e.g. uint16_t.
+       * @param[in] startPtr A pointer to the first byte in the
+       *   message to be decoded.
+       * @param[in] scale A scale value to multiply the integer
+       *   quantity by to get the correct precision of the engineering
+       *   units value before returning.
        * @param[in] shift Number of most significant bits in the bytes
        *   that are not part of the value.  For example, there might
        *   be a 12-bit value stored in the 12 LSBs of a 16-bit word.
@@ -40,17 +74,10 @@ namespace canhacks
        *   to scaling.
        */
    template <typename T>
-   double fixedDecode(uint8_t *startPtr, double scale, unsigned shift, T offset)
+   double fixedDecode(uint8_t *startPtr, double scale, unsigned shift,
+                      T offset)
    {
-      T tmpValInt = *(T*)startPtr;
-      tmpValInt += offset;
-      if (shift)
-      {
-            // bit shift for sign extension or, for unsigned, crud removal
-         tmpValInt <<= shift;
-         tmpValInt >>= shift;
-      }
-      return tmpValInt * scale;
+      return fixedDecode<T>(startPtr, scale, shift, shift, offset);
    }
 }
 
